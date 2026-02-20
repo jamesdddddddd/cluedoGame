86009fb00ce52215a97b780e3e64b4f7bd9ff347
James <jamie.jj.jd@gmail.com>
2026-02-20 15:06:23 +0000

    first changes to introduce multiplayer

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,8 +3,9 @@ using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using System;
 using UnityEngine.Assertions.Must;
+using Unity.Netcode;
 
-public class Movement : MonoBehaviour
+public class Movement : NetworkBehaviour
 {
     // Move speed for the player pieces.
     public float moveSpeed = 5f;
@@ -24,8 +25,11 @@ public class Movement : MonoBehaviour
     private bool logged = false;
 
     // Makes sure that the player is in the designated starting position when the game begins.
-    void Start() 
+    public override void OnNetworkSpawn() 
     {
+        // Search the scene for the diceManager script
+        //this prevents the dice from throwing null execptions
+        DM = GameObject.FindAnyObjectByType<diceManager>();
         transform.position = new Vector3(4.5f, 0.5f, 0.5f);
         whereWeAt();
     }
@@ -33,7 +37,8 @@ public class Movement : MonoBehaviour
     // Checks every frame if the mouse was clicked to initiate movement if valid.
     void Update() 
     {
-    
+        if (!IsSpawned) return;
+
         if (Mouse.current.leftButton.wasPressedThisFrame && !isMoving) 
         {
             checkInput();
