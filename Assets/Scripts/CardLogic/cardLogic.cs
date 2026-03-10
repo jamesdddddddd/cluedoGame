fda919a947b711fb04997234f902240c0acd691f
Neket-laptop <nikita.ziakhor@gmail.com>
2026-03-10 22:25:44 +0000

    card set up

---
+++ Assets/Scripts/CardLogic/cardLogic.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class cardLogic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // we need 6 characrters, 9 rooms, 6 weapons
+    // we will need a sheet of paper to wite down cards that you have, and you found out from other players.
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
